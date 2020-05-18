5d6844b799c7f8d5cb12888b3ae1e22f15bc06cd
ctang13 <ctang13@CTANG13-MOBL5>
2020-05-18 12:16:14 +0800

    add show_task function

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,30 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn cli_show_task() {
+        use crate::cli::show_task;
+        use colored::*;
+        assert_eq!(show_task(0, 0, "test_task"), "test_task".red());
+    }
+}
+
+pub mod cli {
+    use chrono::{Datelike, Local};
+    use colored::*;
+    pub fn show_task(
+        task_begin_week: u32,
+        task_begin_week_day: u32,
+        task_desc: &str,
+    ) -> colored::ColoredString {
+        let now = Local::now();
+        if (now.iso_week().week() * 7 + now.weekday().num_days_from_sunday())
+            >= (task_begin_week * 7 + task_begin_week_day)
+        {
+            task_desc.red()
+        } else {
+            task_desc.blue()
+        }
+    }
 }
